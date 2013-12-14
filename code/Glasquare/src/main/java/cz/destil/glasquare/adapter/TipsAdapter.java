e76328ef507b3c287937c912d8f2ec0f0fa143ff
David Vavra <me@destil.cz>
2013-12-14 14:00:57 +0100

    + autoscaling larger tips (fix #5)

--- code/Glasquare/src/main/java/cz/destil/glasquare/adapter/TipsAdapter.java
+++ code/Glasquare/src/main/java/cz/destil/glasquare/adapter/TipsAdapter.java
@@ -1,20 +1,14 @@
 package cz.destil.glasquare.adapter;
 
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
+import com.google.android.glass.app.Card;
 import com.google.android.glass.widget.CardScrollAdapter;
-import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
-import butterknife.ButterKnife;
-import butterknife.InjectView;
 import cz.destil.glasquare.App;
-import cz.destil.glasquare.R;
 import cz.destil.glasquare.api.Tips;
 import cz.destil.glasquare.util.FormatUtils;
 
@@ -43,22 +37,11 @@ public class TipsAdapter extends CardScrollAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        ViewHolder holder;
-        if (view == null) {
-            view = LayoutInflater.from(App.get()).inflate(R.layout.view_tip, viewGroup, false);
-            holder = new ViewHolder(view);
-            view.setTag(holder);
-        } else {
-            holder = (ViewHolder) view.getTag();
-        }
+            Card card = new Card(App.get());
         Tips.Tip tip = mTips.get(i);
-        holder.text.setText(tip.text);
-        holder.createdAt.setText(FormatUtils.formatDate(tip.createdAt));
-        if (tip.photoUrl != null) {
-            Picasso.with(App.get()).load(tip.photoUrl).into(holder.image);
-        }
-
-        return view;
+        card.setText(tip.text);
+        card.setTimestamp(FormatUtils.formatDate(tip.createdAt));
+        return card.toView();
     }
 
     @Override
@@ -69,18 +52,5 @@ public class TipsAdapter extends CardScrollAdapter {
     @Override
     public int findItemPosition(Object o) {
         return mTips.indexOf(o);
-    }
-
-    static class ViewHolder {
-        @InjectView(R.id.text)
-        TextView text;
-        @InjectView(R.id.createdAt)
-        TextView createdAt;
-        @InjectView(R.id.image)
-        ImageView image;
-
-        public ViewHolder(View view) {
-            ButterKnife.inject(this, view);
-        }
     }
 }
